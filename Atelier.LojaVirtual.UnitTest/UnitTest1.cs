50e52ac9781764720c4f5f97dd7ca538cc790f37
Leandro Azevedo Vilaça <lelecovilaca@hotmail.com>
2015-11-27 14:30:56 -0200

    Primeiro Commit

---
+++ Atelier.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atelier.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestAtelier
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
